2980e4007cfc117e127017ae343db149bbdfad63
karpov-anatolii <schodya@gmail.com>
2024-06-06 23:48:46 +0300

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,9 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pt-20 pb-10 relative" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 bottom-0 min-h-96">
+      <div className="w-full absolute left-0 -top-[300px] min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
